bf53898e08a26564143004f4349bbfac2029e8bb
brandon <brandon@2c0bb46f-6917-0410-8ab9-f4bf1162979e>
2006-09-10 03:52:18 +0000

    cleaned up the tests a bit
    git-svn-id: https://source.collectiveidea.com/public/rails/plugins/acts_as_audited@34 2c0bb46f-6917-0410-8ab9-f4bf1162979e

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,4 +29,24 @@ class Test::Unit::TestCase #:nodoc:
   self.use_instantiated_fixtures  = false
 
   # Add more helper methods to be used by all tests here...
+  
+  # http://project.ioni.st/post/217#post-217
+  #
+  #  def test_new_publication
+  #    assert_difference(Publication, :count) do
+  #      post :create, :publication => {...}
+  #      # ...
+  #    end
+  #  end
+  # 
+  def assert_difference(object, method = nil, difference = 1)
+    initial_value = object.send(method)
+    yield
+    assert_equal initial_value + difference, object.send(method), "#{object}##{method}"
+  end
+  
+  def assert_no_difference(object, method, &block)
+    assert_difference object, method, 0, &block
+  end
+  
 end
