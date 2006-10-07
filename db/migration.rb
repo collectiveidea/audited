0796d31706f4870e8c4f714be2f790bfab52f051
Daniel Morrison <daniel@collectiveidea.com>
2006-10-07 14:55:39 +0000

    Added indexes to the default migration

--- db/migration.rb
+++ db/migration.rb
@@ -7,8 +7,18 @@
       t.column :changes, :text
       t.column :created_at, :datetime
     end
+    
+    add_index :audits, :auditable_id
+    add_index :audits, :auditable_type
+    add_index :audits, :user_id
+    add_index :audits, :created_at  
   end
 
   def self.down
     drop_table :audits
+    
+    remove_index :audits, :auditable_id
+    remove_index :audits, :auditable_type
+    remove_index :audits, :user_id
+    remove_index :audits, :created_at
   end
