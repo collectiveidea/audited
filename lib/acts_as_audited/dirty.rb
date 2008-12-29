c1acd59b1776772010a1fa0b2813fd12d6470be8
Carlos Kozuszko <carlos@insignia4u.com>
2008-12-29 12:55:31 -0200

    - Updating dirty.rb to match the lastest rails implementation of method write_attribute_with_dirty - Also fixing a rails bug that handles incorrectly the changes for null decimal fields: http://www.pastie.org/348281

--- lib/acts_as_audited/dirty.rb
+++ lib/acts_as_audited/dirty.rb
@@ -117,20 +117,32 @@ module CollectiveIdea #:nodoc
           attr = attr.to_s
 
           # The attribute already has an unsaved change.
-          unless changed_attributes.include?(attr)
+          if changed_attributes.include?(attr)
+            old = changed_attributes[attr]
+            changed_attributes.delete(attr) unless field_changed?(attr, old, value)
+          else
             old = clone_attribute_value(:read_attribute, attr)
-
-            # Remember the original value if it's different.
-            typecasted = if column = column_for_attribute(attr)
-                           column.type_cast(value)
-                         else
-                           value
-                         end
-            changed_attributes[attr] = old unless old == typecasted
+            changed_attributes[attr] = old if field_changed?(attr, old, value)
           end
 
           # Carry on.
           write_attribute_without_dirty(attr, value)
+        end
+
+        def field_changed?(attr, old, value)
+          if column = column_for_attribute(attr)
+            if (column.type == :integer || column.type == :decimal) && column.null && (old.nil? || old == 0)
+              # For nullable integer columns, NULL gets stored in database for blank (i.e. '') values.
+              # Hence we don't record it as a change if the value changes from nil to ''.
+              # If an old value of 0 is set to '' we want this to get changed to nil as otherwise it'll
+              # be typecast back to 0 (''.to_i => 0)
+              value = nil if value.blank?
+            else
+              value = column.type_cast(value)
+            end
+          end
+
+          old != value
         end
 
         # def update_with_dirty
