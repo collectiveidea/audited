8ff33b049fa7587ebea781122eccb0b0e21e6d71
Chris Williams <chris@wellnessfx.com>
2012-02-22 11:36:37 -0800

    Add Audit scopes for creates/updates/destroys

--- lib/acts_as_audited/audit.rb
+++ lib/acts_as_audited/audit.rb
@@ -25,6 +25,10 @@ class Audit < ActiveRecord::Base
   default_scope order(:version)
   scope :descending, reorder("version DESC")
 
+  scope :creates,   where(:action => 'create')
+  scope :updates,   where(:action => 'update')
+  scope :destroys,  where(:action => 'destroy')
+
   class << self
 
     # Returns the list of classes that are being audited
