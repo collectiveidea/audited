311db09962d3c9669fc9e3cc067a3106296f87a8
Brian Ryckbost <bryckbost@gmail.com>
2012-04-10 15:26:10 -0400

    Update set_table_name declarations

--- spec/spec_models.rb
+++ spec/spec_models.rb
@@ -11,24 +11,24 @@ class User < ActiveRecord::Base
 end
 
 class CommentRequiredUser < ActiveRecord::Base
-  set_table_name :users
+  self.table_name = :users
   acts_as_audited :comment_required => true
 end
 
 class UnprotectedUser < ActiveRecord::Base
-  set_table_name :users
+  self.table_name = :users
   acts_as_audited :protect => false
   attr_accessible :name, :username, :password
 end
 
 class AccessibleUser < ActiveRecord::Base
-  set_table_name :users
+  self.table_name = :users
   attr_accessible :name, :username, :password # declare attr_accessible before calling aaa
   acts_as_audited
 end
 
 class NoAttributeProtectionUser < ActiveRecord::Base
-  set_table_name :users
+  self.table_name = :users
   acts_as_audited
 end
 
@@ -37,34 +37,34 @@ class Company < ActiveRecord::Base
 end
 
 class Owner < ActiveRecord::Base
-  set_table_name 'users'
+  self.table_name = 'users'
   has_associated_audits
 end
 
 class OwnedCompany < ActiveRecord::Base
-  set_table_name 'companies'
+  self.table_name = 'companies'
   belongs_to :owner, :class_name => "Owner"
   attr_accessible :name, :owner # declare attr_accessible before calling aaa
   acts_as_audited :associated_with => :owner
 end
 
 class OnUpdateDestroy < ActiveRecord::Base
-  set_table_name 'companies'
+  self.table_name = 'companies'
   acts_as_audited :on => [:update, :destroy]
 end
 
 class OnCreateDestroy < ActiveRecord::Base
-  set_table_name 'companies'
+  self.table_name = 'companies'
   acts_as_audited :on => [:create, :destroy]
 end
 
 class OnCreateDestroyExceptName < ActiveRecord::Base
-  set_table_name 'companies'
+  self.table_name = 'companies'
   acts_as_audited :except => :name, :on => [:create, :destroy]
 end
 
 class OnCreateUpdate < ActiveRecord::Base
-  set_table_name 'companies'
+  self.table_name = 'companies'
   acts_as_audited :on => [:create, :update]
 end
 
