3eaa049ddd0cfa0ee83946def07de45951e84a41
Ryan G <ersatzryan@gmail.com>
2012-04-23 14:53:48 -0400

    Extract some commonalities between adapters

---
+++ lib/acts_as_audited/adapters/base/audit.rb
@@ -0,0 +1,107 @@
+module ActsAsAudited
+  module Adapters
+    module Base
+      module Audit
+        def self.included(klass)
+          klass.extend(ClassMethods)
+          klass.setup_audit
+        end
+
+        module ClassMethods
+          def setup_audit
+            belongs_to :auditable,  :polymorphic => true
+            belongs_to :user,       :polymorphic => true
+            belongs_to :associated, :polymorphic => true
+
+            before_create :set_version_number, :set_audit_user
+
+            cattr_accessor :audited_class_names
+            self.audited_class_names = Set.new
+          end
+
+          # Returns the list of classes that are being audited
+          def audited_classes
+            audited_class_names.map(&:constantize)
+          end
+
+          # All audits made during the block called will be recorded as made
+          # by +user+. This method is hopefully threadsafe, making it ideal
+          # for background operations that require audit information.
+          def as_user(user, &block)
+            Thread.current[:acts_as_audited_user] = user
+
+            yieldval = yield
+
+            Thread.current[:acts_as_audited_user] = nil
+
+            yieldval
+          end
+
+          # @private
+          def reconstruct_attributes(audits)
+            attributes = {}
+            result = audits.collect do |audit|
+              attributes.merge!(audit.new_attributes).merge!(:version => audit.version)
+              yield attributes if block_given?
+            end
+            block_given? ? result : attributes
+          end
+
+          # @private
+          def assign_revision_attributes(record, attributes)
+            attributes.each do |attr, val|
+              record = record.dup if record.frozen?
+
+              if record.respond_to?("#{attr}=")
+                record.attributes.has_key?(attr.to_s) ?
+                  record[attr] = val :
+                  record.send("#{attr}=", val)
+              end
+            end
+            record
+          end
+        end
+
+        # Return an instance of what the object looked like at this revision. If
+        # the object has been destroyed, this will be a new record.
+        def revision
+          clazz = auditable_type.constantize
+          (clazz.find_by_id(auditable_id) || clazz.new).tap do |m|
+            self.class.assign_revision_attributes(m, self.class.reconstruct_attributes(ancestors).merge({ :version => version }))
+          end
+        end
+
+        # Returns a hash of the changed attributes with the new values
+        def new_attributes
+          (audited_changes || {}).inject({}.with_indifferent_access) do |attrs,(attr,values)|
+            attrs[attr] = values.is_a?(Array) ? values.last : values
+            attrs
+          end
+        end
+
+        # Returns a hash of the changed attributes with the old values
+        def old_attributes
+          (audited_changes || {}).inject({}.with_indifferent_access) do |attrs,(attr,values)|
+            attrs[attr] = Array(values).first
+
+            attrs
+          end
+        end
+
+        private
+        def set_version_number
+          max = self.class.where(
+            :auditable_id => auditable_id,
+            :auditable_type => auditable_type
+          ).order(:version.desc).first.try(:version) || 0
+          self.version = max + 1
+        end
+
+        def set_audit_user
+          self.user = Thread.current[:acts_as_audited_user] if Thread.current[:acts_as_audited_user]
+          nil # prevent stopping callback chains
+        end
+      end
+    end
+  end
+end
--- lib/acts_as_audited/adapters/mongo_mapper/audit.rb
+++ lib/acts_as_audited/adapters/mongo_mapper/audit.rb
@@ -1,4 +1,5 @@
 require 'set'
+require 'acts_as_audited/adapters/base/audit'
 
 module ActsAsAudited
   module Adapters
@@ -30,14 +31,9 @@ module ActsAsAudited
         key :remote_address, String
         key :created_at, Time
 
-        belongs_to :auditable,  :polymorphic => true
-        belongs_to :user,       :polymorphic => true
-        belongs_to :associated, :polymorphic => true
+        include ActsAsAudited::Adapters::Base::Audit
 
-        before_create :set_version_number, :set_audit_user, :set_created_at
-
-        cattr_accessor :audited_class_names
-        self.audited_class_names = Set.new
+        before_create :set_created_at
 
         scope :ascending,  sort(:version.asc)
         scope :descending, sort(:version.desc)
@@ -46,47 +42,6 @@ module ActsAsAudited
         scope :destroys,   where(:action => 'destroy')
 
         class << self
-          # Returns the list of classes that are being audited
-          def audited_classes
-            audited_class_names.map(&:constantize)
-          end
-
-          # All audits made during the block called will be recorded as made
-          # by +user+. This method is hopefully threadsafe, making it ideal
-          # for background operations that require audit information.
-          def as_user(user, &block)
-            Thread.current[:acts_as_audited_user] = user
-
-            yieldval = yield
-
-            Thread.current[:acts_as_audited_user] = nil
-
-            yieldval
-          end
-
-          # @private
-          def reconstruct_attributes(audits)
-            attributes = {}
-            result = audits.collect do |audit|
-              attributes.merge!(audit.new_attributes).merge!(:version => audit.version)
-              yield attributes if block_given?
-            end
-            block_given? ? result : attributes
-          end
-
-          # @private
-          def assign_revision_attributes(record, attributes)
-            attributes.each do |attr, val|
-              record = record.dup if record.frozen?
-
-              if record.respond_to?("#{attr}=")
-                record.attributes.has_key?(attr.to_s) ?
-                  record[attr] = val :
-                  record.send("#{attr}=", val)
-              end
-            end
-            record
-          end
 
           # @private
           def sanitize_for_time_with_zone(value)
@@ -117,58 +72,24 @@ module ActsAsAudited
             self.username = user
         end
         alias_method :user_as_model=, :user=
-        alias_method :user=, :user_as_string=
+          alias_method :user=, :user_as_string=
 
-        # @private
-        def user_as_string
-          self.user_as_model || self.username
+          # @private
+          def user_as_string
+            self.user_as_model || self.username
         end
         alias_method :user_as_model, :user
         alias_method :user, :user_as_string
 
-        # Return an instance of what the object looked like at this revision. If
-        # the object has been destroyed, this will be a new record.
-        def revision
-          clazz = auditable_type.constantize
-          (clazz.find_by_id(auditable_id) || clazz.new).tap do |m|
-            self.class.assign_revision_attributes(m, self.class.reconstruct_attributes(ancestors).merge({ :version => version }))
-          end
-        end
+
+
 
         # Return all audits older than the current one.
         def ancestors
           self.class.where(:auditable_id => auditable_id, :auditable_type => auditable_type, :version.lte => version)
         end
 
-        # Returns a hash of the changed attributes with the new values
-        def new_attributes
-          (audited_changes || {}).inject({}.with_indifferent_access) do |attrs,(attr,values)|
-            attrs[attr] = values.is_a?(Array) ? values.last : values
-            attrs
-          end
-        end
 
-        # Returns a hash of the changed attributes with the old values
-        def old_attributes
-          (audited_changes || {}).inject({}.with_indifferent_access) do |attrs,(attr,values)|
-            attrs[attr] = Array(values).first
-            attrs
-          end
-        end
-
-      private
-        def set_version_number
-          max = self.class.where(
-              :auditable_id => auditable_id,
-              :auditable_type => auditable_type
-            ).order(:version.desc).first.try(:version) || 0
-          self.version = max + 1
-        end
-
-        def set_audit_user
-          self.user = Thread.current[:acts_as_audited_user] if Thread.current[:acts_as_audited_user]
-          nil # prevent stopping callback chains
-        end
 
         def set_created_at
           self[:created_at] = Time.now.utc if !persisted? && !created_at?
