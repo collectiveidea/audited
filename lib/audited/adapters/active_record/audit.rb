db8d06ba2c9fb803d71f0b2b107879ed48eff1fa
Sean Walbran <swalbran@socialcast.com>
2013-07-10 13:51:48 -0500

    fix calculate-with-conditions deprecation warning

--- lib/audited/adapters/active_record/audit.rb
+++ lib/audited/adapters/active_record/audit.rb
@@ -56,11 +56,7 @@ module Audited
 
       private
         def set_version_number
-          max = self.class.maximum(:version,
-            :conditions => {
-              :auditable_id => auditable_id,
-              :auditable_type => auditable_type
-            }) || 0
+          max = self.class.where(:auditable_id => auditable_id, :auditable_type => auditable_type).maximum(:version) || 0
           self.version = max + 1
         end
       end
