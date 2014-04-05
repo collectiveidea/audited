6f287a0b3d890adf83575cf3623285d8c5bf5730
Daniel Morrison <daniel@collectiveidea.com>
2014-04-05 17:57:43 -0400

    Silence deprecation warnings

--- spec/rails_app/config/application.rb
+++ spec/rails_app/config/application.rb
@@ -1,5 +1,8 @@
 module RailsApp
   class Application < Rails::Application
     config.root = File.expand_path('../../', __FILE__)
+
+    # Silence deprecation warnings
+    config.i18n.enforce_available_locales = false
   end
 end
