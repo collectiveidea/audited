1c6fa684cc6fd907bb5f7bb5e69eacfc515a9fb9
Daniel Morrison <daniel@collectiveidea.com>
2015-09-12 21:53:53 -0400

    Use new hash syntax

--- lib/audited/auditor.rb
+++ lib/audited/auditor.rb
@@ -192,20 +192,20 @@ module Audited
       end
 
       def audit_create
-        write_audit(:action => 'create', :audited_changes => audited_attributes,
-                    :comment => audit_comment)
+        write_audit(action: 'create', audited_changes: audited_attributes,
+                    comment: audit_comment)
       end
 
       def audit_update
         unless (changes = audited_changes).empty? && audit_comment.blank?
-          write_audit(:action => 'update', :audited_changes => changes,
-                      :comment => audit_comment)
+          write_audit(action: 'update', audited_changes: changes,
+                      comment: audit_comment)
         end
       end
 
       def audit_destroy
-        write_audit(:action => 'destroy', :audited_changes => audited_attributes,
-                    :comment => audit_comment) unless self.new_record?
+        write_audit(action: 'destroy', audited_changes: audited_attributes,
+                    comment: audit_comment) unless self.new_record?
       end
 
       def write_audit(attrs)
