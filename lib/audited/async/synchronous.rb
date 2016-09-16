1fa5c38869b191f87175876230976f6f59ddae96
Jim Menard <jim.menard@chloeandisabel.com>
2016-09-16 12:58:20 -0400

    Async Synchronous version param name fix

--- lib/audited/async/synchronous.rb
+++ lib/audited/async/synchronous.rb
@@ -4,7 +4,7 @@ require 'audited/audit'
 module Audited
   module Async
     class Synchronous
-      def self.enqueue(klass, audits_attrs)
+      def self.enqueue(klass_name, audits_attrs)
         klass = Module.const_get(klass_name)
         audits_attrs.each do |attrs|
           klass.create(attrs)
