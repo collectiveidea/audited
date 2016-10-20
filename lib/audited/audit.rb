feba7156a656aa1598cd3071a7d898906e063f5d
Oren Forer <oren@ticketevolution.com>
2016-10-20 12:17:56 -0400

    rename instance attribute version to audit_version
    seems dangerous to dynamically add an accessor method called version
    because version is such a common name and could lead to some
    unexpected behaviour in applications using audited.

--- lib/audited/audit.rb
+++ lib/audited/audit.rb
@@ -46,7 +46,7 @@ module Audited
     def revision
       clazz = auditable_type.constantize
       (clazz.find_by_id(auditable_id) || clazz.new).tap do |m|
-        self.class.assign_revision_attributes(m, self.class.reconstruct_attributes(ancestors).merge(version: version))
+        self.class.assign_revision_attributes(m, self.class.reconstruct_attributes(ancestors).merge(audit_version: version))
       end
     end
 
@@ -105,7 +105,7 @@ module Audited
     def self.reconstruct_attributes(audits)
       attributes = {}
       result = audits.collect do |audit|
-        attributes.merge!(audit.new_attributes)[:version] = audit.version
+        attributes.merge!(audit.new_attributes)[:audit_version] = audit.version
         yield attributes if block_given?
       end
       block_given? ? result : attributes
