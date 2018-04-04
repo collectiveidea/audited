24caa7c578c12a7ff119cc9cd34637b2db13e7dd
Joseph Page <joseph.page@rentacar.fr>
2018-04-04 17:50:48 +0200

    keep old index name + indexes creation at the end
    Signed-off-by: Joseph Page <joseph.page@rentacar.fr>

--- lib/generators/audited/templates/install.rb
+++ lib/generators/audited/templates/install.rb
@@ -1,16 +1,13 @@
 class <%= migration_class_name %> < <%= migration_parent %>
   def change
     create_table :audits do |t|
-      t.belongs_to :auditable, polymorphic: true, index: false
-      t.index [:auditable_type, :auditable_id, :version],
-              name: :index_audits_on_auditable_and_version
+      t.references :auditable, polymorphic: true, index: false
 
       t.references :associated, polymorphic: true,
-                                index: { name: :index_audits_on_associated }
+                                index: { name: 'associated_index' }
 
       t.references :user, type: :<%= options[:audited_user_id_column_type] %>,
-                          polymorphic: true,
-                          index: { name: :index_audits_on_user }
+                          polymorphic: true, index: { name: 'user_index' }
 
       t.string :username
       t.string :action
@@ -18,8 +15,12 @@ class <%= migration_class_name %> < <%= migration_parent %>
       t.integer :version, default: 0
       t.string :comment
       t.string :remote_address
-      t.string :request_uuid, index: true
-      t.datetime :created_at, index: true
+      t.string :request_uuid
+      t.datetime :created_at
+
+      t.index [:auditable_type, :auditable_id, :version], name: 'auditable_index'
+      t.index :request_uuid
+      t.index :created_at
     end
   end
 end
