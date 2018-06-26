9cff81b40fbc71e2455a00ef50f10bc200784af2
Michael Grosser <michael@grosser.it>
2018-06-25 17:23:17 -0700

    fix test failing and being noisy on ruby 2.5

--- spec/rails_app/config/application.rb
+++ spec/rails_app/config/application.rb
@@ -6,3 +6,8 @@ module RailsApp
     config.i18n.enforce_available_locales = true
   end
 end
+
+require 'active_record/connection_adapters/sqlite3_adapter'
+if ActiveRecord::ConnectionAdapters::SQLite3Adapter.respond_to?(:represent_boolean_as_integer)
+  ActiveRecord::ConnectionAdapters::SQLite3Adapter.represent_boolean_as_integer = true
+end
