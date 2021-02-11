f0e6f76cb37914a903b7382af9cc4a990ca7c88a
Patryk Ptasinski <patryk@ipepe.pl>
2021-02-11 18:00:27 +0100

    Added created_at filtering for has_many :audits
    Fix Set problems when accessing audits

--- lib/audited/auditor.rb
+++ lib/audited/auditor.rb
@@ -60,7 +60,13 @@ module Audited
 
         attr_accessor :audit_comment
 
-        has_many :audits, -> { where(service_name: Rails.application.class.parent_name).order(version: :asc) }, as: :auditable, class_name:  
+        has_many :audits, ->(audited_record) do
+          where(
+            service_name: Rails.application.class.parent_name,
+            created_at: (audited_record.created_at - 1.minute)..(Time.now)
+          ).order(version: :asc)
+        end, as: :auditable, class_name: Audited.audit_class.name
+
         Audited.audit_class.audited_class_names << to_s
 
         after_create :audit_create if !options[:on] || (options[:on] && options[:on].include?(:create))
