827defc3ee91bba85c3ae7ceb341c1a7f37ce340
Daniel Morrison <daniel@collectiveidea.com>
2022-12-23 10:45:25 -0500

    More tweaks to get old version tests passing

--- spec/rails_app/config/application.rb
+++ spec/rails_app/config/application.rb
@@ -5,7 +5,7 @@ module RailsApp
     config.root = File.expand_path("../../", __FILE__)
     config.i18n.enforce_available_locales = true
 
-    if config.active_record.respond_to?(:yaml_column_permitted_classes=)
+    if !Rails.version.start_with?("5.0") && !Rails.version.start_with?("5.1") && config.active_record.respond_to?(:yaml_column_permitted_classes=)
       config.active_record.yaml_column_permitted_classes =
         %w[String Symbol Integer NilClass Float Time Date FalseClass Hash Array DateTime TrueClass BigDecimal
           ActiveSupport::TimeWithZone ActiveSupport::TimeZone ActiveSupport::HashWithIndifferentAccess]  
