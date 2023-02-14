4962e204aef793e89d2e1a6b3cc7e547d8ad87bb
Matt Young <mcyoung86@gmail.com>
2023-02-14 13:16:39 -0700

    Make time spec work

--- spec/audited/auditor_spec.rb
+++ spec/audited/auditor_spec.rb
@@ -434,7 +434,7 @@ describe Audited::Auditor do
       it "should store the changed attributes" do
         @user.touch(:suspended_at)
         expect(@user.audits.last.audited_changes["suspended_at"][0]).to be_nil
-        expect(@user.audits.last.audited_changes["suspended_at"][1]).to be_within(1.second).of(Time.current)
+        expect(Time.parse(@user.audits.last.audited_changes["suspended_at"][1].to_s)).to be_within(1.second).of(Time.current)
       end
 
       it "should store audit comment" do
