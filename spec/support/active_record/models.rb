b773b4a62108e8a4cfffb89d63a88b918a0ad96f
Matt Young <mcyoung86@gmail.com>
2023-02-19 11:25:34 -0700

    Don't double audit on touch
    Added specs
    
    
    Handle for update/create
    
    
    Remove unused

--- spec/support/active_record/models.rb
+++ spec/support/active_record/models.rb
@@ -124,11 +124,12 @@ module Models
       audited
       has_associated_audits
       has_many :companies, class_name: "OwnedCompany", dependent: :destroy
+      accepts_nested_attributes_for :companies
     end
 
     class OwnedCompany < ::ActiveRecord::Base
       self.table_name = "companies"
-      belongs_to :owner, class_name: "Owner"
+      belongs_to :owner, class_name: "Owner", touch: true
       attr_accessible :name, :owner if respond_to?(:attr_accessible) # declare attr_accessible before calling aaa
       audited associated_with: :owner
     end
