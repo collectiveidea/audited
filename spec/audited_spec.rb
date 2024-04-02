df41ab138f35e396a9ea6982eda8d5e8291e80c4
Jeremy Anderson <janderson@revela.co>
2024-04-02 11:33:44 -0400

    some better specs

--- spec/audited_spec.rb
+++ spec/audited_spec.rb
@@ -4,10 +4,17 @@ describe Audited do
   describe "#store" do
     describe "maintains state of store" do
       let(:current_user) { Models::ActiveRecord::User.new(name: 'Some User', username: 'some_username') }
-      before { Audited.store[:current_user] = current_user }
+
+      it "can store and retrieve current_user" do
+        expect(Audited.store[:current_user]).to be_nil
+
+        Audited.store[:current_user] = current_user
+
+        expect(Audited.store[:current_user]).to eq(current_user)
+      end
 
       it "checks store is not nil" do
-        expect(Audited.store[:current_user]).to eq(current_user)
+        expect(Audited.store).not_to be_nil
       end
     end
   end
