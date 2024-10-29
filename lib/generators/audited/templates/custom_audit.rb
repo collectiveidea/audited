ad720c55350a072b35aa77b012c8d67f5818cfe4
Andrei Makarov <andrei@kiskolabs.com>
2024-10-29 15:30:54 +0200

    Fix templates for generators to not change amount of spaces and do correct interpolation

--- lib/generators/audited/templates/custom_audit.rb
+++ lib/generators/audited/templates/custom_audit.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 <% table_name = options[:audited_table_name].underscore.pluralize %>
 class <%= table_name.singularize.classify %> < Audited::Audit
   self.table_name = "<%= table_name %>"
