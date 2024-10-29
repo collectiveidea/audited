7cb181671397d4695bc268b4a038814a04c13c62
Andrei Makarov <andrei@kiskolabs.com>
2024-10-29 15:42:34 +0200

    Tune install template to not have `force` option and fix interpolation

--- lib/generators/audited/templates/install.rb
+++ lib/generators/audited/templates/install.rb
@@ -1,7 +1,7 @@
 <%- table_name = options[:audited_table_name].underscore.pluralize -%>
 class <%= migration_class_name %> < <%= migration_parent %>
   def self.up
-    create_table :<%= table_name %>, :force => true do |t|
+    create_table :<%= table_name %> do |t|
       t.column :auditable_id, :integer
       t.column :auditable_type, :string
       t.column :associated_id, :integer
@@ -11,16 +11,16 @@ class <%= migration_class_name %> < <%= migration_parent %>
       t.column :username, :string
       t.column :action, :string
       t.column :audited_changes, :<%= options[:audited_changes_column_type] %>
-      t.column :version, :integer, :default => 0
+      t.column :version, :integer, default: 0
       t.column :comment, :string
       t.column :remote_address, :string
       t.column :request_uuid, :string
       t.column :created_at, :datetime
     end
 
-    add_index :<%= table_name %>, [:auditable_type, :auditable_id, :version], :name => "#{table_name}_auditable_index"
-    add_index :<%= table_name %>, [:associated_type, :associated_id], :name => "#{table_name}_associated_index"
-    add_index :<%= table_name %>, [:user_id, :user_type], :name => "#{table_name}_user_index"
+    add_index :<%= table_name %>, [:auditable_type, :auditable_id, :version], name: "<%= table_name %>_auditable_index"
+    add_index :<%= table_name %>, [:associated_type, :associated_id], name: "<%= table_name %>_associated_index"
+    add_index :<%= table_name %>, [:user_id, :user_type], name: "<%= table_name %>_user_index"
     add_index :<%= table_name %>, :request_uuid
     add_index :<%= table_name %>, :created_at
   end
