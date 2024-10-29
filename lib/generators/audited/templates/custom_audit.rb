39e2ee24e639a410cb47f6de335c69e74cd471ba
Andrei Makarov <andrei@kiskolabs.com>
2024-10-29 14:41:00 +0200

    Implement custom audit model generator within install script

---
+++ lib/generators/audited/templates/custom_audit.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+<% table_name = options[:audited_table_name].underscore.pluralize %>
+class <%= table_name.singularize.classify %> < Audited::Audit
+  self.table_name = "<%= table_name %>"
+end
